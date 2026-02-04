53da58739a280d83a41db910d0184bc5e21fea02
riham <riham.bouchiha@usmba.ac.ma>
2026-02-04 03:03:22 +0100

    refactor: clean up comments in next.config.ts for clarity

--- portfolio-issam/next.config.ts
+++ portfolio-issam/next.config.ts
@@ -1,14 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   eslint: {
-    // Ignore les erreurs ESLint lors du déploiement
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true, // Important
   },
   typescript: {
-    // Ignore les erreurs TypeScript lors du déploiement
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: true, // Important
   },
 };
 
